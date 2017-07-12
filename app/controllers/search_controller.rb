d3872b9206d6f69afd4239aab531f0aa46479c73
Alex Koch <alex549us3@gmail.com>
2017-07-12 10:02:56 -0600

    progress towards api access

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,9 @@
 class SearchController < ApplicationController
   def index
-
+    conn = Faraday.new('https://api.propublica.org') 
+    conn.get do |req|
+      req.url "/congress/v1/members/house/#{params[:state]}/current.json"
+      req.headers['X-API-Key'] = "#{ENV["PROPUBLICA_API_KEY"]}"
+    end
   end
 end
--- spec/features/user_can_search_by_state_spec.rb
+++ spec/features/user_can_search_by_state_spec.rb
@@ -3,7 +3,11 @@ require 'rails_helper'
 describe 'A user can find house of representative members by state' do
   describe 'A user selects a state from the drop down and presses locate members of the house' do
     it 'returns a list of members of the house by selected state' do
+      visit root_path
 
+      select 'Colorado', from: 'state'
+      click_button('Locate Members of the House')
+      expect(page).to have_content('State Representitives For Colorado')
     end
   end
 end
